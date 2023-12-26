9f8d3fec975ed5f5f2c7a6a03b9fe70364b9a02f
javlonartikbaev <artjavlon@gmail.com>
2023-12-26 15:10:31 +0500

    added DELETE and GET_ALL Workers

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, HTTPException, status
 from sqlalchemy.ext.declarative import declarative_base
 from fastapi import FastAPI, Depends, HTTPException
 from database.database import *
@@ -38,3 +40,23 @@ def create_worker(worker: CreateWorker, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_worker)
     return new_worker
+
+
+# ----------------------------------- Список всех работников --------------------------
+
+
+@app.get("/workers/", response_model=List[CreateWorker])
+def get_workers(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    workers = db.query(Workers).offset(skip).limit(limit).all()
+    return workers
+
+
+# Удаление работника
+@app.delete("/workers/{id}", response_model=CreateWorker)
+def delete_worker(worker_id: int, db: Session = Depends(get_db)):
+    worker = db.query(Workers).filter(Workers.idWorkers == worker_id).first()
+    if not worker:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db.delete(worker)
+    db.commit()
+    return worker
