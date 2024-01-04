766900782f33c196155753ca492a50cb4b653783
javlonartikbaev <artjavlon@gmail.com>
2024-01-04 23:34:00 +0500

    2024 01 04

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from models.models import *
 
 DATABASE_URL = "postgresql://root:t2Gcpmf2xUT1iI3AXe5DaJSj5DFjkCvW@dpg-cmb2j96d3nmc73em6f3g-a.singapore-postgres.render.com/databasehh"
 engine = create_engine(DATABASE_URL)
-Base.metadata.drop_all(bind=engine)
+
 Base.metadata.create_all(bind=engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
